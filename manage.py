6f323a4582f85d8781d301c642428d9babdbb27d
Jeff Reiher <jreiher2003@yahoo.com>
2017-01-18 13:14:10 +0000

    Update manage.py with dropbox img script

--- manage.py
+++ manage.py
@@ -23,6 +23,10 @@ def create():
 @manager.command
 def create_dropbox_img():
     Dropbox_Img.__table__.create(db.engine)
+    Dropbox_Img.__table__.create(db.engine)
+    banner = Dropbox_Img(bannerBlue="dropbox_blue.jpg",boxBlue="dropbox_glyph_blue.jpg")
+    db.session.add(banner)
+    db.session.commit()
     print "db img created"
 
 @manager.command 
