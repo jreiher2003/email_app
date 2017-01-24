1a9c1217b2d51987ef60bfc2a327c9eeceb29806
Jeff Reiher <jreiher2003@yahoo.com>
2017-01-24 18:25:29 +0000

    Update bulk_commandline script

--- bulk_flask_email.py
+++ bulk_flask_email.py
@@ -1,16 +1,41 @@
 
 from app import app,db,mail
 from flask import render_template, flash, request, redirect, url_for
+from app.models import Export 
 from flask_mail import Message
 
+def send_email(to, subject, template):
+    msg = Message(
+        subject,
+        recipients=[to],
+        html=template,
+        sender=app.config['MAIL_DEFAULT_SENDER']
+    )
+    mail.send(msg)
+
 users = ["jeffreiher@gmail.com", "jeffreiher@bulletmail.org", "jreiher2003@yahoo.com"]
-with mail.connect() as conn:
-    for i in range(len(users)):
-        message = 'this is test message'
-        subject = "hello, %s" % users[i]
-        msg = Message(recipients=[users[i]],
-                      body=message,
-                      subject=subject)
+def send_bulk():
+    email_list = db.session.query(Export).limit(50000)
+    email_list_50k = db.session.query(Export).offset(50001).limit(50000)
+    email_list_50_100k = db.session.query(Export).offset(100001).limit(50000)
+    email_list_100_150k = db.session.query(Export).offset(150001)
+    with app.app_context():
+        with mail.connect() as conn:
+            for i in email_list:
+                dropbox_url = "https://db.tt/wW7clVpS"
+                html = render_template("dropbox/dropbox_signup.html", dropbox_url=dropbox_url)
+                subject = "Free cloud storage"
+                send_email(i.email, subject, html)
 
-        conn.send(msg)
+def test_query():
+    email_list = db.session.query(Export).limit(50000)
+    email_list_50k = db.session.query(Export).offset(50001).limit(50000)
+    email_list_50_100k = db.session.query(Export).offset(100001).limit(50000)
+    email_list_100_150k = db.session.query(Export).offset(150001)
+    for i in email_list_50k:
+        print i.email
+        
 
+if __name__ == "__main__":
+    send_bulk()
+    # test_query()
