8fcf4fe420892aef04a6238b6dbda4322cc4672d
Jeff Reiher <jreiher2003@yahoo.com>
2017-01-26 13:29:11 +0000

    Move offset to 100

--- bulk_flask_email.py
+++ bulk_flask_email.py
@@ -30,7 +30,7 @@ def gen_list():
     email_list = db.session.query(Export).offset(46000).limit(1000)
 
     """
-    return list(range(142000,190000,1000))
+    return list(range(143000,190000,100))
         
 
 if __name__ == "__main__":
