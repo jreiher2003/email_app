04e4ab184c2e55b50c0035f9654f4d5c730ca9ab
Jeff Reiher <jreiher2003@yahoo.com>
2017-01-26 10:47:49 +0000

    Missed a zero

--- bulk_flask_email.py
+++ bulk_flask_email.py
@@ -30,7 +30,7 @@ def gen_list():
     email_list = db.session.query(Export).offset(46000).limit(1000)
 
     """
-    return list(range(14000,190000,1000))
+    return list(range(140000,190000,1000))
         
 
 if __name__ == "__main__":
